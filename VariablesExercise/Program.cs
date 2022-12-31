e626183434de7f7fc1f681c51f14652b303b2860
adamacneill <adamichelee@gmail.com>
2022-12-30 18:19:30 -0800

    Initial Commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//Declaring and Initializing variables
+string name = "Ada";
+int age = 21;
+char middleInitial = 'M';
+bool isOver18 = true;
+double currentTemp = 33;
+decimal currentPrice = 29.99m;
+
+//Writing the variables to the consoles
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp : {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
